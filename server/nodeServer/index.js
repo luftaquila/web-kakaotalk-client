e5f9532d610c04cea4f2e2b8f659d96b62efae11
LUFT-AQUILA <mail@luftaquila.io>
2020-12-06 17:50:09 +0900

    connection with MariaDB update

--- server/nodeServer/index.js
+++ server/nodeServer/index.js
@@ -1,20 +1,127 @@
-const nodeKakao = require('node-kakao');
-let client = new nodeKakao.TalkClient('SERVER', 'c2VydmVyMQ==');
+require('dotenv').config();
+const express = require('express');
+const mariadb = require('mariadb');
+const winston = require('winston');
+const dateformat = require('dateformat');
+const bodyParser = require('body-parser');
 
-client.login('luftaquila@protonmail.ch', 'rokaFKAk512#', true)
+const pool = mariadb.createPool({
+  host: 'localhost', 
+  user: process.env.DB_User,
+  password: process.env.DB_PW,
+  database: 'kakao',
+  idleTimeout: 0
+});
+
+const nodeKakao = require('node-kakao');
+let client = new nodeKakao.TalkClient(process.env.TalkClientName, process.env.TalkClientUUID);
+
+client.login(process.env.TalkClientLoginID, process.env.TalkClientLoginPW, true)
   .then(main)
   .catch(err => { console.error(`Login Attempt failed. status: ${err.status}, msg: ${err.message}`); });
 
 function main() {
-  console.log('Kakao login success. Main client is on startup.');
-  client.on('message', (chat) => {
-    console.log(chat)
-    console.log(chat.channel.dataStruct.channelId)
-    if(chat.channel.dataStruct.channelId == 174726625044834) {
-      chat.channel.sendText('Kakao login success. Main client is on startup.');
+  console.log('Login successful. Main client is in startup.');
+  
+  /*
+  // Update friends list
+  console.log(client.Auth.getLatestAccessData())
+  client.service.requestFriendList(['normal'], ['create'], client.auth.accessData.accessToken + '-' + client.auth.deviceUUID)
+    .then((res) => { console.log(res) })
+    .catch(err => { console.error(`Login Attempt failed. status: ${err.status}, msg: ${err.message}`); });
+  //client.service.requestProfile().then(res => console.log(res));
+  
+  throw err;
+  */
+  
+  // Incoming message event handler
+  client.on('message', async chat => {
+
+    // if target channel is never seen before
+    
+    let isChannelSeenBefore = await query("SELECT * FROM `chatChannelList` WHERE `channelId`=" + chat.channel.id + ";");
+    if(!isChannelSeenBefore.length) {
+      
+      // insert channel info to `chatChannelList` table
+      await query("INSERT INTO " +
+        "`chatChannelList`(`channelId`, `type`, `activeMemberCount`, `newMessageCount`, `lastChatText`, `lastChatTime`)" +
+        " VALUES(" + chat.channel.id + ", '" + chat.channel.dataStruct.type + "', " + chat.channel.dataStruct.activeMemberCount + ", " + chat.channel.dataStruct.newMessageCount + ", '" + chat.text + "', " + chat.sendTime + ");"
+      );
+      
+      // create channel table
+      await query("CREATE TABLE `" + chat.channel.id + "`(" +
+        "`logId` bigint(20) PRIMARY KEY," + 
+        "`messageId` bigint(20)," + 
+        "`senderId` bigint(20)," +
+        "`sendTime` int(11)," +
+        "`text` text);"
+      );
     }
-    else if(chat.channel.dataStruct.channelId == 205241728180694) {
-      chat.channel.sendText('Incoming message detected. content: ' + chat.text);
+    
+    // if target sender is never seen before
+    if(chat.sender.id != 246786864 && !await query("SELECT * FROM `friendsList` WHERE `userId`=" + chat.sender.id + ";").length) {
+      console.log('new user detected');
+      // request friend info
+      let profile = await client.service.requestProfile(chat.sender.id);
+      let info = await client.service.findFriendById(chat.sender.id);
+      
+      // insert user info to `friendsList` table
+      await query("INSERT INTO " +
+        "`friendsList`(`userId`, `name`, `profileImageurl`, `lastSeenAt`, `directChatChannelId`)" +
+        " VALUES(" + chat.sender.id + ", '" + info.friend.nickName + "', '" + info.friend.profileImageUrl + "', " + profile.lastSeenAt + ", 0);"
+      );
+    }
+    
+    // add chat log
+    await query("INSERT INTO " +
+      "`" + chat.channel.id + "`(`logId`, `messageId`, `senderId`, `sendTime`, `text`)" +
+      " VALUES(" + chat.logId + ", '" + chat.messageId + "', " + chat.sender.id + ", " + chat.sendTime + ", '" + chat.text + "');"
+    );
+    
+    // update lastchat info
+    await query("UPDATE `chatChannelList` SET `lastChatText`='" + chat.text + "', `lastChatTime`='" + chat.sendTime + "' WHERE `channelId`=" + chat.channel.id + ";");
+    
+    
+    if(chat.sender.id != 246786864 && chat.channel.dataStruct.type == 'DirectChat') {
+      //chat.channel.sendText('Incoming message detected. content: ' + chat.text);
+      if(chat.channel.dataStruct.channelId == 174726625044834) { } // 최서원
+      //else if(chat.channel.dataStruct.channelId == 205241728180694) { } // 박세진
+      else {
+        chat.markChatRead();
+        let delay = Math.random();
+        setTimeout(() => {
+          chat.channel.sendText('Incoming message detected.\nContent: ' + chat.text + '\nReply delay: ' + Math.round(delay * 1000) + 'ms');
+        }, delay * 1000);
+      }
     }
   });
+  
+  
+  // Message read event handler
+  client.on('message_read', (channel, reader, readChatLogId) => {
+    /*
+    console.log('----------------channel-------------------');
+    console.log(channel)
+    */
+    //console.log(reader)
+    console.log(readChatLogId)
+    
+  });
+  
 }
+
+async function query(queryString) {
+  let db, result;
+  try {
+    db = await pool.getConnection();
+    result = await db.query(queryString);
+  }
+  catch(err) { throw err; }
+  finally {
+    if(db) db.end();
+    console.log(queryString)
+    console.log(result)
+    return result;
+  }
+}
+  
--- server/pythonClient/RegisterDevice.py
+++ server/pythonClient/RegisterDevice.py
@@ -5,13 +5,30 @@ user_pw = 'rokaFKAk512#' #str(input("PW: "))
 
 device_name = "SERVER"
 user_uuid = "c2VydmVyMQ=="
-
+'''
 httpApi.RequestPasscode(user_id, user_pw, device_name, user_uuid)
 
 passcode = str(input("Input Passcode : "))
 
 res = httpApi.RegisterDevice(user_id, user_pw, device_name, user_uuid, passcode)
 print(res)
-
+'''
 res = httpApi.Login(user_id, user_pw, device_name, user_uuid)
 print(res)
+
+{
+  "status":0,
+  "userId":246786864,
+  "countryIso":"KR",
+  "countryCode":"82",
+  "accountId":97954701,
+  "server_time":1607222527,
+  "resetUserData":false,
+  "access_token":"3080a561f69c47bc80b68f3e72f8c78800000016072116486120020durQXyg11J",
+  "refresh_token":"62a98adc18d643129e261d1ef18f3e2800000016072116486120020rpBGTzTixz-CBrS2Q",
+  "token_type":"bearer",
+  "autoLoginAccountId":"luftaquila@protonmail.ch",
+  "displayAccountId":"luftaquila@protonmail.ch",
+  "mainDeviceAgentName":"android",
+  "mainDeviceAppVersion":"9.1.3"
+}
